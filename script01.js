f7c0e71f1ec8171bd1f75f88e4438e786dca9aa3
alanwarthon <familiawarthon@gmail.com>
2025-09-14 13:39:54 -0700

    Tercera fase

--- script01.js
+++ script01.js
@@ -4,7 +4,9 @@ const usernameField = document.getElementById("txt01");
 const weightField = document.getElementById("txt02");
 const heightField = document.getElementById("txt03");
 const btnProcess = document.getElementById("btnCalculate");
-const txtMessageField = document.getElementById("txtS");
+const lblMessageField = document.getElementById("lblIMC");
+const welcomeName = document.getElementById("welcomeName");
+const img = document.getElementById("imgHuman");
 
 btnProcess.addEventListener("click",calculateIMC);
 
@@ -17,6 +19,17 @@ function calculateIMC(){
   let IMC = (weight/(height*height))
   console.log(IMC);
 
-  let finalMsg = `Welcome ${userName}, your IMC is ${IMC.toFixed(2)}`;
-  txtMessageField.textContent = finalMsg;
+  let finalMsg = `IMC = ${IMC.toFixed(2)}`;
+  lblMessageField.textContent = finalMsg;
+  welcomeName.textContent = `Welcome ${userName}`;
+
+  if(IMC > 30.0){
+    img.src="images/obesidad.png";
+  } else if(IMC > 25.0 && IMC < 29.9){
+    img.src="images/supNormal.png";
+  }else if(IMC > 18.5 && IMC < 24.9){
+    img.src="images/normal.png";
+  }else if(IMC < 18.5){
+    img.src="images/infNormal.png";
+  }
 }
